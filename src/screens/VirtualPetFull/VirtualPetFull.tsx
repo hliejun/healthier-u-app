c202fe87e59c24bd367a3d181573a9eed23c7dc8
Huang Lie Jun <liejun.huang@ufinity.com>
2024-06-05 20:17:41 +0800

    feat(enhancement): add lottie

--- src/screens/VirtualPetFull/VirtualPetFull.tsx
+++ src/screens/VirtualPetFull/VirtualPetFull.tsx
@@ -1,9 +1,17 @@
+import LottieView from 'lottie-react-native';
+import React from 'react';
 import { Text, View } from 'react-native';
 
 export const VirtualPetFull = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Virtual Pet Full</Text>
+      <LottieView
+        source={require('../../../assets/lottie/ottie/walk.json')}
+        style={{ width: '100%', height: '50%' }}
+        autoPlay
+        loop
+      />
     </View>
   );
 };
