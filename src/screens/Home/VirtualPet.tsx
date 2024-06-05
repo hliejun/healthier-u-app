0da0e5bd00c2fa298abf21cafb5f24fcf5534167
Jun Jie <choo_jun_jie@tech.gov.sg>
2024-06-05 19:03:53 +0800

    [JJ] Added virtual pet metrics and styling

--- src/screens/Home/VirtualPet.tsx
+++ src/screens/Home/VirtualPet.tsx
@@ -1,46 +1,265 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
-export const VirtualPet = () => {
-  return (
-    <View style={styles.virtualPetSection}>
-      <View style={styles.virtualPetContent}>
-        <Text style={styles.virtualPet}>INSERT TAMACHIGO HERE!</Text>
-      </View>
-    </View>
-  );
+const VirtualPet = () => {
+	return (
+		<View style={styles.virtualPetSection}>
+			<View style={styles.imageContainer}>
+				<View style={styles.imageSection}>
+					<Image
+						source={require('../../../assets/images/background.png')}
+						style={styles.petImage}
+					/>
+				</View>
+				<View style={styles.iconContainer}>
+					<View style={styles.iconBox}>
+						<Ionicons name={'settings-outline'} size={24} color={'#FFFFFF'} />
+					</View>
+					<View style={styles.iconBox}>
+						<Ionicons name={'medkit-outline'} size={24} color={'#FFFFFF'} />
+					</View>
+					<View style={styles.iconBox}>
+						<Ionicons name={'shirt-outline'} size={24} color={'#FFFFFF'} />
+					</View>
+				</View>
+			</View>
+
+			<View style={styles.statsContainer}>
+				<View style={styles.statBox}>
+					<View style={styles.statGroup}>
+						<Text style={styles.statText}>Calories</Text>
+						<Text style={styles.statValue}>1,000 / 1,800</Text>
+					</View>
+					<View style={styles.progressBarContainer}>
+						<View style={styles.iconBoxCalories}>
+							<Ionicons
+								name={'restaurant-outline'}
+								size={16}
+								color={'white'}
+							/>
+						</View>
+						<View
+							style={[
+								styles.progressBar,
+								{ backgroundColor: '#6C63FF', width: '55.56%' },
+							]}
+						/>
+					</View>
+				</View>
+
+				<View style={styles.statBox}>
+					<View style={styles.statGroup}>
+						<Text style={styles.statText}>Steps</Text>
+						<Text style={styles.statValue}>1,000 / 5,000</Text>
+					</View>
+					<View style={styles.progressBarContainer}>
+						<View style={styles.iconBoxSteps}>
+							<Ionicons name={'walk-outline'} size={16} color={'white'} />
+						</View>
+						<View
+							style={[
+								styles.progressBar,
+								{ backgroundColor: '#1DA1F2', width: '20%' },
+							]}
+						/>
+					</View>
+				</View>
+
+				<View style={styles.statBox}>
+					<View style={styles.statGroup}>
+						<Text style={styles.statText}>MVPA</Text>
+						<Text style={styles.statValue}>30 / 30 mins</Text>
+					</View>
+					<View style={styles.progressBarContainer}>
+						<View style={styles.iconBoxMVPA}>
+							<Ionicons name={'bicycle-outline'} size={16} color={'white'} />
+						</View>
+						<View
+							style={[
+								styles.progressBar,
+								{ backgroundColor: '#FFD700', width: '40%' },
+							]}
+						/>
+					</View>
+				</View>
+
+				<View style={styles.statBox}>
+					<View style={styles.statGroup}>
+						<Text style={styles.statText}>Sleep</Text>
+						<Text style={styles.statValue}>2.5 / 8 hours</Text>
+					</View>
+					<View style={styles.progressBarContainer}>
+						<View style={styles.iconBoxSleep}>
+							<Ionicons name={'moon-outline'} size={16} color={'white'} />
+						</View>
+						<View
+							style={[
+								styles.progressBar,
+								{ backgroundColor: '#4CAF50', width: '31.25%' },
+							]}
+						/>
+					</View>
+				</View>
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  virtualPetSection: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    backgroundColor: '#FFFFFF',
-    borderRadius: 4,
-    maxWidth: '100%',
-    marginTop: 16,
-    marginHorizontal: 16,
-    alignItems: 'center',
-    height: 300,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
-    borderCurve: 'circular',
-  },
-  virtualPetContent: {
-    flex: 2,
-    backgroundColor: '#FFFFFF',
-    height: '100%',
-    justifyContent: 'center',
-    padding: 10,
-  },
-  virtualPet: {
-    textAlign: 'center',
-    fontSize: 16,
-    fontFamily: 'Inter-SemiBold',
-  },
+	virtualPetContent: {
+		flex: 2,
+		backgroundColor: '#FFFFFF',
+		height: '100%',
+		justifyContent: 'center',
+		padding: 10,
+	},
+	virtualPet: {
+		textAlign: 'center',
+		fontSize: 16,
+		fontFamily: 'Inter-SemiBold',
+	},
+	virtualPetSection: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'flex-start',
+		backgroundColor: '#FFFFFF',
+		borderRadius: 4,
+		maxWidth: '100%',
+		alignItems: 'flex-start',
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.23,
+		shadowRadius: 2.62,
+		elevation: 4,
+		borderTopRightRadius: 24,
+		borderTopLeftRadius: 24,
+		display: 'flex',
+		marginTop: 16,
+		marginHorizontal: 16,
+		height: '100%',
+		borderCurve: 'circular',
+	},
+	imageSection: {
+		flex: 1,
+		width: '100%',
+	},
+	imageContainer: {
+		flex: 1,
+		width: '100%',
+		height: 230,
+		top: -7,
+		position: 'relative',
+	},
+	petImage: {
+		width: '100%',
+		height: '100%',
+		borderTopRightRadius: 34,
+		borderTopLeftRadius: 34,
+	},
+	iconContainer: {
+		position: 'absolute',
+		left: 10,
+		top: '20%',
+		zIndex: 1,
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		height: '60%',
+	},
+	iconBox: {
+		backgroundColor: '#5b5b5b',
+		opacity: 20,
+		borderRadius: 4,
+		padding: 8,
+		marginBottom: 16,
+	},
+	statsContainer: {
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		justifyContent: 'space-between',
+		padding: 10,
+	},
+	statBox: {
+		width: '48%',
+		marginBottom: 16,
+		backgroundColor: '#FFFFFF',
+		padding: 16,
+		borderRadius: 8,
+		shadowColor: '#000',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.23,
+		shadowRadius: 2.62,
+		elevation: 4,
+	},
+	statGroup: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	statText: {
+		fontSize: 12,
+		fontFamily: 'Inter-Bold',
+		marginBottom: 4,
+		color: '#333333',
+	},
+	statValue: {
+		fontSize: 12,
+		fontFamily: 'Inter-SemiBold',
+		marginBottom: 4,
+		color: '#333333',
+	},
+	progressBarContainer: {
+		width: '100%',
+		height: 10,
+		marginTop: 10,
+		backgroundColor: '#E0E0E0',
+		borderRadius: 4,
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	progressBar: {
+		height: '100%',
+		borderRadius: 4,
+	},
+	iconBoxCalories: {
+		width: 24,
+		height: 24,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginRight: 4,
+		backgroundColor: '#6C63FF',
+		borderRadius: 16,
+	},
+	iconBoxSteps: {
+		width: 24,
+		height: 24,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginRight: 4,
+		backgroundColor: '#1DA1F2',
+		borderRadius: 16,
+	},
+	iconBoxMVPA: {
+		width: 24,
+		height: 24,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginRight: 4,
+		backgroundColor: '#FFD700',
+		borderRadius: 16,
+	},
+	iconBoxSleep: {
+		width: 24,
+		height: 24,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginRight: 4,
+		backgroundColor: '#4CAF50',
+		borderRadius: 16,
+	},
 });
 
 export default VirtualPet;
